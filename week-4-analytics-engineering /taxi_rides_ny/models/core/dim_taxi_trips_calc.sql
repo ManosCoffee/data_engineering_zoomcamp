1b150bdd54b7bac39afbfd3cce2e9b8b36e750df
Manos Palaiologos <manoscoffee@gmail.com>
2025-02-22 15:48:32 +0000

    updates 22.02

---
+++ week-4-analytics-engineering /taxi_rides_ny/models/core/dim_taxi_trips_calc.sql
@@ -0,0 +1,22 @@
+{{
+    config(
+        materialized='view'
+    )
+}}
+
+with fct_trips as (
+    select 
+        *,
+        extract(year from pickup_datetime)  as rev_year,
+        extract(month from pickup_datetime)  as rev_month,
+        extract(quarter from pickup_datetime) as rev_quarter,
+    from {{ref("fact_trips")}}
+)
+
+select *,    
+    {{ dbt.safe_cast("rev_year", api.Column.translate_type("string")) }} || "-Q" || {{ dbt.safe_cast("rev_quarter", api.Column.translate_type("string")) }} as year_quarter
+    from fct_trips
+
+
+
+
