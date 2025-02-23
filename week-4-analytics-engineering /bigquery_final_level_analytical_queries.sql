93dd0f33b2ddd5ae89793e0f103798f2f5903d71
Emmanouil Palaiologos <manoscoffee@gmail.com>
2025-02-23 16:25:59 +0200

    Update bigquery_final_level_analytical_queries.sql

--- week-4-analytics-engineering /bigquery_final_level_analytical_queries.sql
+++ week-4-analytics-engineering /bigquery_final_level_analytical_queries.sql
@@ -1,4 +1,4 @@
--- Final level analytical queries on materialized tables of BigQuery transformed with DBT
+
 
 -- Q5 : MAX and MIN YoY for Green & Yellow taxis --
 
@@ -23,3 +23,10 @@ left join `kestra-orch.zoomcamp.fct_taxi_trips_quarterly_revenue` fmax
   on fmax.quartertly_yoy_rev = mm.max_yoy
 left join `kestra-orch.zoomcamp.fct_taxi_trips_quarterly_revenue` fmin                   
   on fmin.quartertly_yoy_rev = mm.min_yoy
+
+  
+-- Q5 : Get p90, p95 & p97 percentiles per Green/Yellow taxis, for April 2020
+
+select * from `kestra-orch.zoomcamp.fct_taxi_trips_monthly_fare_p95` 
+where rev_year = 2020 
+and rev_month = 4;
