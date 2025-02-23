53494ba992526d07f7a551f084b6d07af39b842c
Emmanouil Palaiologos <manoscoffee@gmail.com>
2025-02-24 01:23:32 +0200

    Update bigquery_final_level_analytical_queries.sql

--- week-4-analytics-engineering /bigquery_final_level_analytical_queries.sql
+++ week-4-analytics-engineering /bigquery_final_level_analytical_queries.sql
@@ -22,11 +22,45 @@ from max_min_q_yoy mm
 left join `kestra-orch.zoomcamp.fct_taxi_trips_quarterly_revenue` fmax                   
   on fmax.quartertly_yoy_rev = mm.max_yoy
 left join `kestra-orch.zoomcamp.fct_taxi_trips_quarterly_revenue` fmin                   
-  on fmin.quartertly_yoy_rev = mm.min_yoy
+  on fmin.quartertly_yoy_rev = mm.min_yoy;
 
   
--- Q5 : Get p90, p95 & p97 percentiles per Green/Yellow taxis, for April 2020
+-- Q6 : Get p90, p95 & p97 percentiles per Green/Yellow taxis, for April 2020
 
 select * from `kestra-orch.zoomcamp.fct_taxi_trips_monthly_fare_p95` 
 where rev_year = 2020 
 and rev_month = 4;
+
+
+--  Q7:  Rank p90-duration values within each 3-dimension partition:
+--  within each pickup zone for each month & year, rank the drop-off locations by p90 duration 
+
+WITH ranked_p90_durations_per_period_and_region AS (
+    SELECT 
+        *,
+    DENSE_RANK() OVER (PARTITION BY trip_year, trip_month, pickup_locationid
+        ORDER BY p90 DESC) p90_duration_rank
+    FROM  `kestra-orch.zoomcamp.fct_fhv_monthly_zone_traveltime_p90` 
+
+
+)
+
+
+SELECT  
+  array_agg(distinct dropoff_zone) dropoff_zones
+FROM ranked_p90_durations_per_period_and_region
+WHERE trip_year = 2019
+AND trip_month = 11
+AND pickup_zone in (
+    "Newark Airport",
+    "SoHo",
+    "Yorkville East"
+)
+AND p90_duration_rank = 2
+group by trip_year, trip_month, pickup_zone
+
+
+
+
+
+
